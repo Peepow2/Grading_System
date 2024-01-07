3ce422c46eedfc448db71da56aeb0dee168dc3a1
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-01-08 00:38:57 +0700

    Update ID.py

--- ID.py
+++ ID.py
@@ -1,22 +1,6 @@
 from random import randint
-
-def reset_pass():
-    p = open('pass_init.txt', 'w')
-    p.write('0')
-    p.close()
-    return 
-
-def saved_pass():
-    p = open('pass_init.txt', 'r')
-    I = int(p.readline()) + int(randint(3, 10))
-    p.close()
-    p = open('pass_init.txt', 'w')
-    p.write(str(I))
-    p.close()
-    return I
-
-def get_ID():    
-    I = saved_pass()
+def get_ID(I):
+    I += int(randint(3, 10))
     code = ('000' + str(I))[-4:]
     check = sum([int(e) for e in code]) % 10
     return '683' + code + str(check)
