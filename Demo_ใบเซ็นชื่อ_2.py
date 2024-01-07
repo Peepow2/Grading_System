ebc5fd2c300759184fca2b3669b957ee4c133db2
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-01-08 00:41:13 +0700

    Update Demo_ใบเซ็นชื่อ_2.py

--- Demo_ใบเซ็นชื่อ_2.py
+++ Demo_ใบเซ็นชื่อ_2.py
@@ -12,7 +12,7 @@ pdf.set_right_margin(15)
 pdf.add_font('Th_sarabun_psk', '', 'THSarabun.ttf')
 pdf.add_font('Th_sarabun_psk', 'B', 'THSarabun Bold.ttf')
 
-ID.reset_pass()
+
 HEADER = ["ลำดับ", "เลขที่นั่งสอบ", "รหัสประจำตัวสอบ", "ชื่อ - นามสกุล", "ลายมือชื่อ"]
 DATA =  ["นายแมนยู ดูแล้วแพ้", "นายลิเวอร์พูล แพ้บ๊วยคูล ๆ",
          "นายเซลซี มีแต่เงิน", "นายอาร์เซนอล ราย่าบอลหลุด",
@@ -20,19 +20,19 @@ DATA =  ["นายแมนยู ดูแล้วแพ้", "นายล�
          "นายมาดริด พิชิตสี่แชมป์", "นายบาร์ซ่า มาแล้วคับ",
          ]
 size = (10, 25, 30, 65, 40)
-
-NO_Room = 'x 401 402 404 405 501 502 504 505 601 602'.split()
+NO_Room = '401 402 404 405 501 502 504 505'.split()
 R = 1
 seq = 0
 B = 35
-N = 321
+N = int(1000)
 Page = 0
+prev = 0
 while R <= N:
     if R % 35 == 1:
         seq = 1
         Page += 1
-        
         pdf.add_page()
+        
         pdf.set_font('Th_sarabun_psk', 'B', 14)
         pdf.cell(180, 7, 'ใบเซ็นชื่อผู้เข้าสอบ', border = 0, align = 'C')
         pdf.ln()
@@ -48,7 +48,7 @@ while R <= N:
         pdf.cell(50, 7, 'เวลาสอบ: 8:30 – 11:30 น', border = 0, align = 'L')
         pdf.ln()
         pdf.cell(120, 7, 'สนามสอบ: จุฬาลงกรณ์มหาวิทยาลัย', border = 0, align = 'L')
-        pdf.cell(50, 7, f'ห้องสอบที่: {Page} ({NO_Room[Page]})', border = 0, align = 'L')
+        pdf.cell(50, 7, f'ห้องสอบที่: {Page} ({NO_Room[Page%8]})', border = 0, align = 'L')
         pdf.ln(10)
 
         pdf.set_line_width(0.2)
@@ -61,13 +61,16 @@ while R <= N:
     with pdf.table(width = 180, line_height = 6, first_row_as_headings = False, col_widths = size, \
                    text_align=("CENTER", "CENTER", "CENTER", "LEFT", "CENTER")) as table:
         while True:
+            stu_ID = ID.get_ID(prev)
+            prev = int(stu_ID[3:-1])
+            
+            ROW = [str(R), str(((R-1) % B) + 1), stu_ID, DATA[(R-1) % 8], '']
             row = table.row()
-            ROW = [str(R), str(((R-1) % B) + 1), ID.get_ID(), DATA[(R-1) % 8], '']
             for datum in ROW:
                 row.cell(datum)
             R += 1
             seq += 1
-            if seq >= B: break
+            if seq >= B or R >= N: break
 
 pdf.output("Simple.pdf") # Create ใบรายชื่อ
 # ----------------------------------------------- # 
@@ -76,5 +79,6 @@ if PRINT: os.startfile("Simple.pdf", "print")
 # ----------------------------------------------- # 
 print("Finished")
 total = time.time() - ss
+print(total)
 print('TIME USED =', int(total // 3600), 'hour', int(total // 60), 'min', int(total % 60) + 1, 'seconds')
 #os.remove("Simple.pdf")
