dcaf504c61bca964ad81b776560f1b9287c9cf1c
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-01-08 22:45:49 +0700

    Update ID.py

--- ID.py
+++ ID.py
@@ -1,6 +1,9 @@
 from random import randint
 def get_ID(I):
-    I += int(randint(3, 10))
-    code = ('000' + str(I))[-4:]
+    I = int(I[3:-1]) + int(randint(4, 13))
+    code = ('0000' + str(I))[-4:]
     check = sum([int(e) for e in code]) % 10
     return '683' + code + str(check)
+
+def Rint(a, b):
+    return int(randint(a, b))
