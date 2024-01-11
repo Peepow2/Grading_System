88cb8b0c89a97320ef1ec05e2ead1edfd2aac967
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-01-11 13:42:10 +0700

    Add files via upload

---
+++ Demo/convert_data_to_json.py
@@ -0,0 +1,21 @@
+import JSON_DICT_convertor as JDC
+
+fin = open('Name_ID_gen.txt', 'r')
+DATA = dict()
+for line in fin.readlines():
+    line = line.split()
+    ID = line[0]
+    Name = line[1] + ' ' + line[2]
+    if ID not in DATA:
+        DATA[ID] = dict()
+    DATA[ID]['Name'] = Name.strip()
+    DATA[ID]['score'] = 60.0
+    DATA[ID]['ROOM'] = '301'
+fin.close()
+JDC.Dict2JSON(DATA, 'DATA.json')
+
+DATA2 = JDC.JSON2Dict('DATA.json')
+print(DATA2)
+
+
+
