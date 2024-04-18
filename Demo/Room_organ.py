7c946b88df7e65f9b0a831334cd8becaa6a13bad
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-04-19 03:26:21 +0700

    Update Room_organ.py

--- Demo/Room_organ.py
+++ Demo/Room_organ.py
@@ -2,24 +2,26 @@ def Room_organization(Seq_Name):
     Special_Room = 2
     NO_ROOM = 8
     Per_Room = (len(Seq_Name) // NO_ROOM) + int((len(Seq_Name) % NO_ROOM) != 0)
-    ROOM = [list() for i in range(NO_ROOM)]
-
-    Room_Name = ['King', 'Queen', 'J', 'A', '10', '9', '8', '7']
-    Number_Room = '205 206 207 208 201 202 203 204'.split()
-    Seq_Room = [int(e) for e in range(Special_Room + 1, 9)]
-
+    ROOM = dict()
+    NNR = "204 Priest,205 Bard,206 Archer,301 Scorcerer,304 Warlock,306 Rogue,404 Druid,409 Barbarian"
+    Number_Name_Room = [tuple(e.strip().split()) for e in NNR.split(',')]
+    
+    for e in Number_Name_Room:
+        ROOM[e[0]] = list()
+    
     for i in range(len(Seq_Name)):
         if i < (Per_Room * Special_Room):
-            ROOM[i % Special_Room].append(Seq_Name[i])
+            ROOM[Number_Name_Room[i % Special_Room][0]].append(Seq_Name[i])
         else:
-            ROOM[Seq_Room[i % (8 - Special_Room)] - 1].append(Seq_Name[i])
+            ROOM[Number_Name_Room[(i % (8 - Special_Room)) + Special_Room][0]].append(Seq_Name[i])
+            
+    for line in ROOM:
+        print(line)
+        print(ROOM[line])
+# ------------------------------------------------------------------------- #
+Seq_Name = [('00' + str(e))[-3::] for e in range(1, 35)]
+Room_organization(Seq_Name)
 
-    fout = open('List_ROOM.txt', 'w')
-    for i in range(len(ROOM)):
-        #print(Number_Room[i], '-->' ,ROOM[i])
-        fout.write(str(Number_Room[i]) + ' --> ' + str(ROOM[i]) + '\n')
-    fout.close()
-    return
 # ------------------------------------------------------------------------- #
 Seq_Name = [('00' + str(e))[-3::] for e in range(1, 270)]
 Room_organization(Seq_Name)
