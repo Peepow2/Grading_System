be9848b5d25c2c6c821c8387e6f9b9c4a9cd2694
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-04-20 16:34:53 +0700

    Update Room_organ.py

--- Demo/Room_organ.py
+++ Demo/Room_organ.py
@@ -4,30 +4,29 @@ def Room_organization(Seq_Name):
     NO_ROOM = 8
     Per_Room = (len(Seq_Name) // NO_ROOM) + int((len(Seq_Name) % NO_ROOM) != 0)
     ROOM = dict()
-    NR = "204,Priest,205,Bard,206,Archer,301,Scorcerer,304,Warlock,306,Rogue,404,Druid,409,Barbarian".split(',')
+    RN = "204,Priest,205,Bard,206,Archer,301,Scorcerer,304,Warlock,306,Rogue,404,Druid,409,Barbarian".split(',')
     seq = [int(i) for i in range(8)]
     random.shuffle(seq)
 
-    for i in range(1, len(NR), 2):
-        name = NR[i]
-        ROOM[name] = list()
+    for i in range(1, len(RN), 2):
+        ROOM[RN[i]] = list()        # RN[i] = room_namee
     
     for i in range(len(Seq_Name)):
         if i < (Per_Room * Special_Room):
             idx = seq[i % Special_Room]
         else:
             idx = seq[(i % (8 - Special_Room)) + Special_Room]
-        r = NR[2*idx + 1]
+        r = RN[2*idx + 1]
         ROOM[r].append(Seq_Name[i])
     
     fout = open("List_ROOM.txt", 'w')
-    for room_nam in ROOM:
-        room_num = NR[NR.index(room_nam) - 1]
-        print(room_nam, (9 - len(room_nam)) * ' ' , \
-             '(' + room_num + ')', '-->', ROOM[room_nam])
+    for room_name in ROOM:
+        room_num = RN[RN.index(room_name) - 1]
+        print(room_name, (9 - len(room_name)) * ' ' , \
+             '(' + room_num + ')', '-->', ROOM[room_name])
         
-        fout.write(room_nam + (9 - len(room_nam)) * ' ' + \
-             ' (' + room_num + ')' + ' --> ' + str(ROOM[room_nam]) + '\n')
+        fout.write(room_name + (9 - len(room_name)) * ' ' + \
+             ' (' + room_num + ')' + ' --> ' + str(ROOM[room_name]) + '\n')
     fout.close()
     return
 # ------------------------------------------------------------------------- #
